3a86e1ee63622665065e38662e67287930f5c05d
den-m1985 <denismartyanov244@gmail.com>
2022-10-29 15:01:59 +0200

    Програмки для телеграм бота

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import telebot
-#import config
+import config
 
-#bot = telebot.TeleBot(config.TOKEN)
-bot = telebot.TeleBot('5731423337:AAGr97IwINO9S7VXf20PIx7JpGjU-IZzXow')
+bot = telebot.TeleBot(config.TOKEN)
+#bot = telebot.TeleBot('5731423337:AAGr97IwINO9S7VXf20PIx7JpGjU-IZzXow')
 
-@bot.message_handler(content_types=['text'])
-def lalala(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=["start"])
+def lalala(m, res=False):
+    bot.send_message(m.chat.id, 'message.text')
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'вы написали')
     
 # run
-bot.polling(non_stop=True)
+bot.polling(non_stop=True, interval=0)
