befbfec3a5e069c3c9bfdebb71aae4505b7211a6
den-m1985 <denismartyanov244@gmail.com>
2023-01-01 22:58:40 +0200

    бот с кнопками

--- bot4.py
+++ bot4.py
@@ -9,11 +9,26 @@ bot = TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn1, btn2)
-    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я тестовый бот для твоей статьи для habr.com".format(message.from_user), reply_markup=markup)
-    
+    #btn1 = types.KeyboardButton("👋 Поздороваться")
+    info = types.KeyboardButton("❓ INFO")
+    abc = types.KeyboardButton("Действие")
+    markup.add(info, abc)
+    bot.send_message(message.chat.id, text="Привет user".format(message.from_user), reply_markup=markup)
 
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if(message.text == "❓ INFO"):
+        bot.send_message(message.chat.id, text="Информация")
+    elif(message.text == "Действие"):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = types.KeyboardButton("Добавить новый контакт?")
+        btn2 = types.KeyboardButton("Вывести на экран контакты?")
+        back = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(btn1, btn2, back)
+        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+
+    elif message.text == "Добавить новый контакт?":
+        bot.send_message(message.chat.id, text="Ипалит")
 
 bot.polling(none_stop=True, interval=0)
